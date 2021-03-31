48b62c7a3e20bf9953eb93e0b4afd60b5d8b0a2a
seidmesetovic <seid.mesetovic@gmail.com>
2021-03-31 09:07:53 +0200

    ex05 gotov

---
+++ ex05/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import SimpleForm from './SimpleForm';
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleForm />
+    </div>
+  );
+}
+
+export default App;
